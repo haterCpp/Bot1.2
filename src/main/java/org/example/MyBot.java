ba5d297afce70976d81d0583892edb76dff92607
haterCpp <121799732+haterCpp@users.noreply.github.com>
2024-02-20 11:20:00 +0200

    Update MyBot.java
    Add bot command "all"

--- src/main/java/org/example/MyBot.java
+++ src/main/java/org/example/MyBot.java
@@ -12,32 +12,43 @@ public class MyBot extends TelegramLongPollingBot {
     }
 
     @Override
-    public void onUpdateReceived(Update update) {
-        var chatId = update.getMessage().getChatId();
-        var text =update.getMessage().getText();
-        try {
-        var message = new SendMessage();
-if(text.equals("/start")){
-    message.setText("Hello!");
-}else if (text.equals("btc")) {
-var price= CryptoPrice.spotPrice("BTC");
-    message.setText("BTC price: " + price.getAmount().doubleValue());
-}else if (text.equals("eth")) {
-    var price= CryptoPrice.spotPrice("ETH");
-    message.setText("ETH price: " + price.getAmount().doubleValue());
-}else if (text.equals("doge")) {
-    var price= CryptoPrice.spotPrice("Doge");
-    message.setText("DogeCoin price: " + price.getAmount().doubleValue());
-}else {
-    message.setText("Unknown command!");
-}
-        message.setChatId(chatId);
+public void onUpdateReceived(Update update) {
+    var chatId = update.getMessage().getChatId();
+    var text = update.getMessage().getText();
 
-            execute(message);
-        } catch (Exception e){
-            System.out.println("Error");
+    try {
+        if (text.equals("/start")) {
+            sendMessage(chatId, "Hello!");
+        } else if (text.equals("btc")) {
+            //sendPicture(chatId, "Bitcoin.png");
+            sendPrice(chatId, "BTC");
+        } else if (text.equals("eth")) {
+            sendPrice(chatId, "ETH");
+        } else if (text.equals("doge")) {
+            sendPrice(chatId, "Doge");
+        } else if (text.equals("all")) {
+            sendPrice(chatId, "BTC");
+            sendPrice(chatId, "ETH");
+            sendPrice(chatId, "Doge");
+        } else {
+            sendMessage(chatId, "Unknown command!");
         }
+    } catch (Exception e) {
+        System.out.println("Error!");
     }
+}
+
+void sendPrice(long chatId, String name) throws Exception {
+    var price = CryptoPrice.spotPrice(name);
+    sendMessage(chatId, name + " price: " + price.getAmount().doubleValue());
+}
+
+void sendMessage(long chatId, String text) throws Exception {
+    var message = new SendMessage();
+    message.setChatId(chatId);
+    message.setText(text);
+    execute(message);
+}
     @Override
     public String getBotUsername() {
         return "Crying_java_4_bot";
